85edd63b3c7bc72face14c6fc1cfa3f95372c0ea
José María Moreno <kiruco.info@gmail.com>
2021-03-08 12:38:44 +0100

    test folder

---
+++ src/miscellaneous.ts
@@ -0,0 +1,9 @@
+export interface ResponseObject {
+  message: string
+  statusCode: number
+}
+
+export let increaseHTTPRequestsCounter = () => {
+  return HTTPRequestsCounter++;
+}
+export let HTTPRequestsCounter: number = 0;
